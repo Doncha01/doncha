4da1bbfc2782d386622e93201da36e82bbb9d402
Damir Alimbekov <Alimbekovdamir35@gmail.com>
2020-05-07 16:00:00 +0700

    New Uber (07.05.2020)

--- uber/js/script.js
+++ uber/js/script.js
@@ -1,17 +1,17 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const header__nav = document.querySelector('.header__nav'),
-    headerItem = document.querySelectorAll('.header__item'),
-    hamburger = document.querySelector('.hamburger');
+    const menu = document.querySelector('.menu'),
+          menuItem = document.querySelectorAll('.menu_item'),
+          hamburger = document.querySelector('.hamburger');
 
     hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger__active');
-        header__nav.classList.toggle('header__active');
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
     });
 
-    headerItem.forEach(item => {
+    menuItem.forEach(item => {
         item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger__active');
-            header__nav.classList.toggle('header__active');
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
         })
     })
 })
